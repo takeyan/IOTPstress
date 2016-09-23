a853e9ebc4d26162056681a9a4636e8a6ef1d3c1
Seiichi Takeuchi <takeyan.g@gmail.com>
2016-09-23 07:18:23 +0000

    Parameters are defined at the beginning of sub98.js.

--- sub98.js
+++ sub98.js
@@ -1,24 +1,29 @@
 var Client = require("ibmiotf");
-var deviceInfo = require("./demo01_demo03.json");     // デバイス定義
 
-// メッセージ件数カウント用配列
-var device = new Array();
-for (var i=0 ; i<deviceInfo.length ; i++ ){
-    device[deviceInfo[i].deviceId] = 0;
-}
-
-// メッセージ受信時刻保存用配列
-var recTime = new Array();
+var org = "bf8dsi";				//IOTP組織ID
+var apikey = "a-bf8dsi-klpbvn9ant";     // API Key
+var apiauth =  "wZAkUucqxklGaCStuZ";    // 認証トークン
+var deviceInfo = require("./dev101_110.json");     // デバイス定義
 
 var stflag = true;
-var startTime = null;
+var startTime = new Date();
+var size = 2048; // payloadのサイズのデフォルト値（スループット計算用）
+
+
+// メッセージ件数カウント用配列および受信時刻保存用配列の初期化
+var device = new Array();
+var recTime = new Array();
+for (var i=0 ; i<deviceInfo.length ; i++ ){
+    device[deviceInfo[i].deviceId] = 0;
+    recTime[deviceInfo[i].deviceId] = new Date();
+}
 
 // IOTPに接続
 var appClientConfig = {
-            "org" : 'm7llwg',
+            "org" : org,
             "id" : 'sub01',
-            "auth-key" : 'a-m7llwg-muuqrsoctk',
-            "auth-token" : '?drGWsWSWv-ZYOMTqO',
+            "auth-key" : apikey,
+            "auth-token" : apiauth,
             "type" : "shared" // Enables Shared Subscription Mode
     }
     var appClient = new Client.IotfApplication(appClientConfig);
@@ -27,8 +32,8 @@ var appClientConfig = {
     appClient.on("connect", function () {
 	console.log("application connected to IOT Platform");
           appClient.subscribeToDeviceEvents("demo01");
-          appClient.subscribeToDeviceEvents("demo02");
-          appClient.subscribeToDeviceEvents("demo03");
+//          appClient.subscribeToDeviceEvents("demo02");
+//          appClient.subscribeToDeviceEvents("demo03");
 //          appClient.subscribeToDeviceStatus("demo03");
     });
 
@@ -38,12 +43,11 @@ var appClientConfig = {
 appClient.on("deviceEvent", function (deviceType, deviceId, eventType, format, payload) {
      if(stflag){ 
 		startTime = new Date(); 
+		size = payload.length;
 		stflag = false;
 		}
      device[deviceId]++;
      recTime[deviceId] = new Date();
-
-// console.log("Message from " + deviceId + " is arrived. payload is: " + payload);
 });
 
 appClient.on("error", function (err) {
@@ -51,36 +55,35 @@ appClient.on("error", function (err) {
 });
 
 // 一定間隔で進捗表示
-var loop = setInterval(function(){
-     var totalCount = 0;
-     var lastTime = startTime;
-     console.log("\n");
-     for ( var i = 0; i < deviceInfo.length ; i++ ) {
-          console.log("[" + deviceInfo[i].deviceId + "] 受信件数：" + device[deviceInfo[i].deviceId] + "件.");
-	  totalCount = totalCount + device[deviceInfo[i].deviceId];
-	  lastTime = ( lastTime > recTime[deviceInfo[i].deviceId] ) ? lastTime : recTime[deviceInfo[i].deviceId] ;
-          }
-     var elapsedTime = lastTime - startTime;
-     var recRate = totalCount / elapsedTime * 1000;
-     var thruput = recRate * 2 / 1024 ; // MB/sec
-     console.log("総受信件数：" + totalCount + "[件]. 所用時間：" + elapsedTime.toFixed(2) + "[msec]. 受信レート：" + recRate.toFixed(2) + "[件/秒] スループット：" + thruput.toFixed(2) + "[MB/秒]");
-		},5000);
-
-
+var loop = setInterval(function(){statMon(null)},5000);
 
 // CTRL+Cで終了時の集計処理
-process.on("SIGINT", function(){
+process.on("SIGINT", function(){statMon("SIGINT")});
+
+     
+// 進捗集計用コールバック関数     
+var statMon = function(ev){
      var totalCount = 0;
      var lastTime = startTime;
      console.log("\n");
      for ( var i = 0; i < deviceInfo.length ; i++ ) {
           console.log("[" + deviceInfo[i].deviceId + "] 受信件数：" + device[deviceInfo[i].deviceId] + "件.");
 	  totalCount = totalCount + device[deviceInfo[i].deviceId];
-	  lastTime = ( lastTime > recTime[deviceInfo[i].deviceId] ) ? lastTime : recTime[deviceInfo[i].deviceId] ;
+	  lastTime = ( lastTime >= recTime[deviceInfo[i].deviceId] ) ? lastTime : recTime[deviceInfo[i].deviceId] ;
           }
      var elapsedTime = lastTime - startTime;
-     var recRate = totalCount / elapsedTime * 1000;
-     var thruput = recRate * 2 / 1024 ; // MB/sec
-     console.log("総受信件数：" + totalCount + "[件]. 所用時間：" + elapsedTime.toFixed(2) + "[msec]. 受信レート：" + recRate.toFixed(2) + "[件/秒]. スループット：" + thruput.toFixed(2) + "[MB/秒]");
-     process.exit(0);
-     });
+     var recRate = (elapsedTime === 0) ? 0 : totalCount / elapsedTime * 1000;
+     var thruput = recRate * size / 1024 / 1024 ; // MB/sec
+     console.log("開始時刻：" + dateFmt(startTime) + " 最終時刻：" + dateFmt(lastTime) );
+     console.log("総受信件数：" + totalCount + "[件]. 経過時間：" + elapsedTime.toFixed(2) + "[msec]. 受信レート：" + recRate.toFixed(2) + "[件/秒] スループット：" + thruput.toFixed(2) + "[MB/秒]");
+
+    if (ev === "SIGINT") {
+        process.exit(0);
+    }
+}
+
+
+// 日付フォーマット用関数
+function dateFmt(dt){
+    return dt.toISOString().replace(/T/," ").replace(/Z/,"");
+}
